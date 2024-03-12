eec435ce94c122c5672b849d03dfea62fe3ed80e
DanielftD2 <dfelipet1213@gmail.com>
2024-03-12 17:00:10 -0500

    xd

--- app/src/main/java/co/edu/unipiloto/login/SolicitudActivity.java
+++ app/src/main/java/co/edu/unipiloto/login/SolicitudActivity.java
@@ -11,7 +11,7 @@ public class SolicitudActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitud);
     }
-    public void gola(){
+    public void hola(){
 
     }
 }
