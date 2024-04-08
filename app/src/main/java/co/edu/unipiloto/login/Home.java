0b8bf87772df6be360e8f2d2d2c829ff7741550e
BuildTools <unconfigured@null.spigotmc.org>
2024-04-07 23:50:45 -0500

    Cambio de registro de contraseña a que sea Hasheada, Información del usuario en Perfil, Al registrar una nueva solicitud se actualiza la array de "Mis Publicaciones", tambien cuando loguea compara con un compare_hash, arregle la imagen de la navbar deslizante. Cambie archivos de php y agregué uno nuevo, agregue nueva clase de AdapterPublicacionesPerfil para las publicaciones del Perfil y agregué un nuevo layout del mismo diseño de las publicaciones. Agregué UserManager para administrar el usuario desde una clase

--- app/src/main/java/co/edu/unipiloto/login/Home.java
+++ app/src/main/java/co/edu/unipiloto/login/Home.java
@@ -14,68 +14,94 @@ import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class Home extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Home extends AppCompatActivity implements AdapterPublicaciones.OnConductorButtonClickListener {
 
     private DrawerLayout drawerLayout;
     NestedScrollView nestedScrollView;
     RecyclerView recyclerView;
     ProgressBar progressBar;
-    static int contador=0;
-    static int aux=0;
-    static List<String> listaPublicaciones = new ArrayList<>();
+    static int contador = 0;
+    static RequestQueue requestQueue;
+    private AdapterPublicaciones adapter;
+    private static List<Publicacion> listaPublicaciones = new ArrayList<>();
+    String correo;
+    boolean deslizo = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        correo =getIntent().getStringExtra("usuario");
 
-        drawerLayout=findViewById(R.id.drawer_layout);
+        requestQueue = Volley.newRequestQueue(this);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
 
         nestedScrollView = findViewById(R.id.nsvVista);
         recyclerView = findViewById(R.id.rvUsuarios);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
         progressBar = findViewById(R.id.pbCargando);
 
         ObtenerPublicacion(recyclerView,Home.this);
+        //  if(deslizo){
         nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
             @Override
+
             public void onScrollChange(@NonNull NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                if (scrollY == v.getChildAt(0).getMeasuredHeight()-v.getMeasuredHeight()){
+                if (scrollY == v.getChildAt(0).getMeasuredHeight() - v.getMeasuredHeight()) {
                     contador++;
                     progressBar.setVisibility(View.VISIBLE);
-
+                    deslizo = true;
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            if (contador<10){
-                                ObtenerPublicacion(recyclerView, Home.this);
+                            if (contador < 10) {
+
+                                ObtenerPublicacion(recyclerView,Home.this);
                             }
                         }
-                    },3000);
+                    }, 3000);
                 }
             }
         });
+        // }
+
+        adapter = new AdapterPublicaciones(this,listaPublicaciones);
+        adapter.setOnConductorButtonClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
     public static void ObtenerPublicacion(RecyclerView recyclerView, Context context) {
-        for (int i=0;i<10;i++){
-            aux++;
-            listaPublicaciones.add("Usuario con ID"+aux);
-        }
-        recyclerView.setAdapter(new AdapterPublicaciones(context, listaPublicaciones));
+        readUser(recyclerView, context);
     }
 
     private boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int itemId=item.getItemId();
-        switch (itemId){
+        int itemId = item.getItemId();
+        switch (itemId) {
             case R.id.nav_Home:
                 startActivity(new Intent(this, Home.class));
                 return true;
@@ -83,7 +109,7 @@ public class Home extends AppCompatActivity {
                 startActivity(new Intent(this, FavoritosActivity.class));
                 return true;
             case R.id.nav_formulario:
-                startActivity(new Intent(this,SolicitudActivity.class));
+                startActivity(new Intent(this, SolicitudActivity.class));
                 return true;
             case R.id.nav_perfil:
                 startActivity(new Intent(this, PerfilActivity.class));
@@ -91,5 +117,55 @@ public class Home extends AppCompatActivity {
             default:
                 return false;
         }
+    }
+
+    private static void readUser(RecyclerView recyclerView, Context context) {
+        String URL = "http://192.168.56.1/rodo/traerPublicaciones.php";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            listaPublicaciones.clear();
+                            for (int i=0;i<response.length();i++) {
+                                JSONObject userObject = response.getJSONObject(i);
+                                Publicacion publicacion = new Publicacion();
+                                publicacion.setNombre("producto: "+userObject.getString("name"));
+                                publicacion.setOrigen("Origen: "+userObject.getString("origen"));
+                                publicacion.setDestino("Destino: "+userObject.getString("destino"));
+                                publicacion.setPeso("Peso: "+userObject.getInt("peso"));
+                                publicacion.setDescripcion("Descripcion: "+userObject.getString("detalles"));
+                                publicacion.setFecha(userObject.getString("fecha"));
+                                publicacion.setIdPublicacion(userObject.getInt("id"));
+                                listaPublicaciones.add(publicacion);
+                            }
+                            recyclerView.setAdapter(new AdapterPublicaciones(context,listaPublicaciones));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    @Override
+    public void onConductorButtonClick(int position) {
+        Publicacion publicacion= listaPublicaciones.get(position);
+
+        agregarConductor(publicacion);
+    }
+
+    private void agregarConductor(Publicacion publicacion) {
+
     }
 }
